0604a914f9287380f918e3c70879bfa3ca27c916
Veronika Sosolikova <v_sosolikova@utb.cz>
2024-02-12 21:22:13 +0100

    add function fetchCurrencyData()

--- app/src/main/java/cz/utb/fai/tictactoe/SecondActivity.kt
+++ app/src/main/java/cz/utb/fai/tictactoe/SecondActivity.kt
@@ -2,10 +2,40 @@ package cz.utb.fai.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import cz.utb.fai.tictactoe.databinding.ActivityMainBinding
+import java.net.HttpURLConnection
+import java.net.URL
+import android.widget.TextView
+
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        fetchCurrencyData().start()
+    }
+
+    private fun fetchCurrencyData(): Thread
+    {
+        return Thread {
+
+            val url = URL("https://open.er-api.com/v6/latest/czk")
+            val connection = url.openConnection() as HttpURLConnection
+
+            if(connection.responseCode == 200)
+            {
+                val inputSystem = connection.inputStream
+                println(inputSystem.toString())
+            }
+            else
+            {
+                binding.baseCurrency.text = "Failed Connection"
+            }
+        }
+
     }
 }
